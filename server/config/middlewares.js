5aacdf548d994cdbbcff34ebc0e172d5adf7fd7e
Raisul <raisulconnects@gmail.com>
2026-08-30 11:20:46 +0600

    CORS Management for Deployment

--- server/config/middlewares.js
+++ server/config/middlewares.js
@@ -1,8 +1,28 @@
-module.exports = [
+module.exports = ({ env }) => [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com', 'dl.airtable.com', '*'],
+          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com', 'dl.airtable.com', '*'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept', 'X-Requested-With'],
+      origin: env.array('CORS_ORIGIN', ['http://localhost:3000', 'http://localhost:1337', '*']),
+      credentials: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
@@ -10,3 +30,4 @@ module.exports = [
   'strapi::favicon',
   'strapi::public',
 ];
+
